e377da018122d1e64132411596b4ae08bfb1f655
BENILAUTYZ5667 <benitezlauti234@gmail.com>
2025-03-26 16:58:09 -0300

    Create loading.js

---
+++ loading.js
@@ -0,0 +1,38 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+    [3289], {
+        13762: function(e, s, t) {
+            Promise.resolve().then(t.bind(t, 46608))
+        },
+        46608: function(e, s, t) {
+            "use strict";
+            t.r(s), t.d(s, {
+                default: function() {
+                    return c
+                }
+            });
+            var n = t(57437),
+                r = t(16620);
+            let i = [{
+                id: 0,
+                src: "/search/hotels/lottie/member.svg"
+            }, {
+                id: 1,
+                src: "/search/hotels/lottie/building.svg"
+            }, {
+                id: 2,
+                src: "/search/hotels/lottie/room-service.svg"
+            }];
+
+            function c() {
+                return (0, n.jsx)(r.Z, {
+                    images: i
+                })
+            }
+        }
+    },
+    function(e) {
+        e.O(0, [9512, 6620, 2971, 2117, 1744], function() {
+            return e(e.s = 13762)
+        }), _N_E = e.O()
+    }
+]);
